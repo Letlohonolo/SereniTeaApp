4074626d8e86313decf0bfb7fb579b11fecf49ce
ST10375622 <st10375622@vcconnect.edu.za>
2025-09-11 15:08:44 +0200

    navigation bar created

--- app/src/main/java/com/fake/sereniteaapp/HomeActivity.kt
+++ app/src/main/java/com/fake/sereniteaapp/HomeActivity.kt
@@ -4,16 +4,26 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private lateinit var db: FirebaseFirestore
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navView: NavigationView
+    private lateinit var toolbar: Toolbar
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,10 +48,39 @@ class HomeActivity : AppCompatActivity() {
                 }
         }
 
-        findViewById<Button>(R.id.btnLogout).setOnClickListener {
-            auth.signOut()
-            startActivity(Intent(this, LoginActivity::class.java))
-            finish()
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navView = findViewById(R.id.nav_view)
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        //directs the user to the specific screen
+        toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        setupNavigation()
+    }
+
+    //enables the users navigation
+    private fun setupNavigation() {
+        navView.setNavigationItemSelectedListener { menuItem ->
+            drawerLayout.closeDrawer(GravityCompat.START)
+            when (menuItem.itemId) {
+                R.id.journal -> startActivity(Intent(this, HomeActivity::class.java))
+                R.id.motivation -> startActivity(Intent(this, HomeActivity::class.java))
+                R.id.progress -> startActivity(Intent(this, HomeActivity::class.java))
+                R.id.garden -> startActivity(Intent(this, HomeActivity::class.java))
+                R.id.settings -> startActivity(Intent(this, HomeActivity::class.java))
+                R.id.signOut -> {
+                    auth.signOut()
+                    startActivity(Intent(this, LoginActivity::class.java))
+                    finish()
+                    true
+                }
+                R.id.home ->  recreate()
+                else -> false
+            }
+            true
         }
     }
 }
